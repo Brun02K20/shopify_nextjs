673520aac11ae2b0b0e644273d8e8b7a8c1d05fe
Brun02K23 <bvirinni@gmail.com>
2024-05-20 12:34:00 -0300

    ruta dinamica de categorias y search param

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -1,14 +1,18 @@
 // como esoy usando Typescript, debo declarar las propiedades que reciba mi componente
 interface CategoryProps {
     params: {
-        category: string
-    }
+        categories: string[]
+    },
+    searchParams: {
+        search?: string;
+    };
 }
 
 export default function Category(props: CategoryProps) {
-    const {category} = props.params
+    const {categories} = props.params
+    console.log(categories)
 
     return (
-        <h1>Categoria: {category}</h1>
+        <h1>Categoria: {categories}</h1>
     )
 }
