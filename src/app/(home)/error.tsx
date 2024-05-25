cd5309a99befb99e9d44fce18ba1bf417c52519f
Brun02K23 <bvirinni@gmail.com>
2024-05-24 21:33:26 -0300

    error.tsx

---
+++ src/app/(home)/error.tsx
@@ -0,0 +1,27 @@
+"use client"
+import styles from "./error.module.sass"
+import { useEffect } from "react"
+
+
+
+
+// para reintentar la peticion en caso de que ocurra un error y se renderice esto
+interface ErrorProps {
+    error: Error
+    reset: () => void
+}
+
+export default function Error({error, reset}: ErrorProps) {
+    // una vez se termine de montar el componente de error
+    useEffect(() => {
+        console.log(error)
+    }, [])
+
+    return (
+        <div className={styles.ErrorContainer}>
+            <h1>:(</h1>
+            <p>Oops, Something went wrong</p>
+            <button onClick={reset}>RETRY</button>
+        </div>
+    )
+}
