a695b6deef86c6926127e855b6baa8e5ec0bafaa
Brun02K23 <bvirinni@gmail.com>
2024-05-26 17:05:12 -0300

    fix bug from products and redirect if id doesn´t exists

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,5 +1,6 @@
 import { ProductView } from "app/components/product/ProductView";
 import { getProducts } from "app/services/shopify/products";
+import { redirect } from "next/navigation";
 
 interface ProductPageProps {
     searchParams: {
@@ -13,6 +14,10 @@ export default async function ProductPage({searchParams}: ProductPageProps) {
     const product = products[0]
     console.log("producto: ", product);
     
+    if (!id) {
+        redirect("/store")
+    }
+
     return (
         <ProductView product={product} />
     )
