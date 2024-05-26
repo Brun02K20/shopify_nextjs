0cefa6564d7544075ca80a76a26ccfd7261481f0
Brun02K23 <bvirinni@gmail.com>
2024-05-26 17:28:23 -0300

    metadata a nivel de compoennte

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,13 @@
 import { MainProducts } from "app/components/Home/MainProducts";
+import { Metadata } from "next";
+
+// metadatos para estaticas
+export const metadata: Metadata = {
+  title: "Future World",
+  description: "Welcome to Future World Store, an ecommerce from other century",
+  keywords: ["ecommerce", "future", "world"]
+
+}
 
 export default function Home() {
   return (
--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,10 +1,29 @@
 import { ProductView } from "app/components/product/ProductView";
 import { getProducts } from "app/services/shopify/products";
 import { redirect } from "next/navigation";
+import { title } from "process";
 
 interface ProductPageProps {
     searchParams: {
         id: string
+    }
+}
+
+// para metadatos dinamicos
+export async function generateMetadata({searchParams}: ProductPageProps) {
+    const id = searchParams.id
+    const products = await getProducts(id)
+    const product = products[0]
+
+    return {
+        title: product.title,
+        description: product.description,
+        keywords: product.tags,
+        openGraph: {
+            images: [
+                product.image
+            ]
+        }
     }
 }
 
